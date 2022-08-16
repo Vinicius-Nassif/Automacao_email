5ae0b0fb6443b506289acde88412301f419d270d
Vinicius-Nassif <vinicius.nassif@gmail.com>
2022-08-16 17:00:54 -0300

    commit inicial

---
+++ bulk-word-generator.py
@@ -0,0 +1,40 @@
+from pathlib import Path
+
+from docx2pdf import convert     #pip install docx2pdf
+import pandas as pd 	#pip install pandas openpyxl
+from docxtpl import DocxTemplate 	#pip install docxtpl
+
+base_dir = Path(__file__).parent
+word_template_path = base_dir / "vert_contato.docx"
+excel_path = base_dir / "contacts-list.xlsx"
+output_dir = base_dir / "OUTPUT"
+
+
+## Criar pasta output para os documentos word
+# Create output folder for the word documents
+output_dir.mkdir(exist_ok=True)
+
+
+## Converter planilha do Excel em Pandas Dataframe
+# Convert Excel sheet into pandas dataframe
+df = pd.read_excel(excel_path, sheet_name="Sheet1")
+
+
+##  Exibir somente data YYYY-MM-DD (sem o horário)
+# Keep only date part YYYY-MM-DD (not the time)
+df["DATA_ENTREGA"] = pd.to_datetime(df["DATA_ENTREGA"]).dt.date
+
+
+## Alterando o formato da data de YYYY-MM-DD para DD-MM-YYYY
+# Changing format date YYYY-MM-DD to DD-MM-YYYY
+df["DATA_ENTREGA"] = pd.to_datetime(df["DATA_ENTREGA"]).dt.strftime('%d/%m/%Y')
+
+
+## Interação entre o Excel e a produção dos documentos Word
+# Interate over each row in df and render word document 
+for record in df.to_dict(orient="records"):
+	doc = DocxTemplate(word_template_path)
+	doc.render(record)
+	output_path = output_dir / f"{record['NOME_DESTINATARIO']} - vert_contato.docx"
+	doc.save(output_path)
+	convert( output_path, f"{output_dir}/{record['NOME_DESTINATARIO']}.pdf")
---
+++ single-word-generator.py
@@ -0,0 +1,21 @@
+import datetime
+from pathlib import Path
+
+
+from docxtpl import DocxTemplate # pip install docxtpl
+
+
+base_dir = Path(__file__).parent
+word_template_path = base_dir / "vert_contato.docx"
+
+doc = DocxTemplate(word_template_path)
+context = {
+	"NOME_DESTINATARIO": "André Almeida fontes",
+	"ENDEREÇO": "Rua Alameda dos santos, número 5",
+	"CEP": "65232-232",
+	"DATA_ENTREGA": "23/05/2021",
+	"NUMERO_OC": "1200323134",
+	"PRODUTO": "TECLADO GAMER XL",
+}
+doc.render(context)
+doc.save(base_dir / "vert_contato_generated.docx")
